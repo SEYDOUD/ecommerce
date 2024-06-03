b5c361d14ddcbeebe7c9486fd172731bf92a51b4
SEYDOUD <SEYDOUDANSOGO00@gmail.com>
2024-06-03 07:33:19 +0000

    Just setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,html}", // Ajoutez ici toutes les extensions de fichiers pertinents
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
